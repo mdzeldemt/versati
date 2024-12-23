5f2f22044bc1f8865a95251618d4de6ad661343e
Facundo Almeida <facundoalmeida@gmail.com>
2024-12-23 20:49:17 -0300

    Add `getEntry` operation to Miniflux API client

--- versati/miniflux/src/main/kotlin/com/liuvil/versati/api/retrofit.kt
+++ versati/miniflux/src/main/kotlin/com/liuvil/versati/api/retrofit.kt
@@ -1,20 +1,25 @@
 package com.liuvil.versati.api
 
 import com.liuvil.versati.api.data.EntriesResponse
+import com.liuvil.versati.api.data.Entry
 import com.liuvil.versati.api.data.Feed
 import com.liuvil.versati.api.data.SortDirection
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface MinifluxRetrofitApi: MinifluxApi {
-
-    @GET("/v1/feeds")
-    override suspend fun getFeeds(): List<Feed>
 
     @GET("/v1/entries")
     override suspend fun getEntries(
         @Query("direction") direction: SortDirection?,
         @Query("limit") limit: Int?
     ): EntriesResponse
+
+    @GET("/v1/entries/{id}")
+    override suspend fun getEntry(@Path("id") id: Int): Entry
+
+    @GET("/v1/feeds")
+    override suspend fun getFeeds(): List<Feed>
 
 }
