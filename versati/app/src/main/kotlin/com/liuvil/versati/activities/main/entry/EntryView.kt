b4d20ed146a69eb4aeb742fd81598e284ffa7f0b
Facundo Almeida <facundoalmeida@gmail.com>
2024-12-24 17:38:32 -0300

    Update title and enclosure appearance in entry view

--- versati/app/src/main/kotlin/com/liuvil/versati/activities/main/entry/EntryView.kt
+++ versati/app/src/main/kotlin/com/liuvil/versati/activities/main/entry/EntryView.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
@@ -20,6 +21,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import coil3.compose.AsyncImage
 import com.liuvil.versati.framework.android.openURLExternally
@@ -61,18 +64,20 @@ fun EntryView(
             ) {
                 Text(
                     it.title,
+                    fontSize = 20.sp,
                     fontWeight = FontWeight.Bold,
                     modifier = Modifier.clickable(
                         onClick = openURL,
                         indication = null,
                         interactionSource = null
-                    )
+                    ).padding(8.dp)
                 )
 
                 enclosure?.let { enclosure ->
                     AsyncImage(
                         model = enclosure.url.toString(),
-                        contentDescription = null
+                        contentDescription = null,
+                        modifier = Modifier.padding(horizontal = 8.dp)
                     )
                 }
 
