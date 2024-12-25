893a35793c9681670d7eaf3f5fadc79d16970f30
Facundo Almeida <facundoalmeida@gmail.com>
2024-12-25 16:16:39 -0300

    Add confirmation dialog to mark page as read

--- versati/app/src/main/kotlin/com/liuvil/versati/activities/main/MainScreen.kt
+++ versati/app/src/main/kotlin/com/liuvil/versati/activities/main/MainScreen.kt
@@ -14,12 +14,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.liuvil.versati.activities.main.entry_list.EntryListView
+import com.liuvil.versati.components.ConfirmationDialog
 import com.liuvil.versati.framework.viewmodel.State
 import com.liuvil.versati.framework.viewmodel.bindViewModel
 import kotlinx.coroutines.launch
@@ -34,6 +37,7 @@ fun MainScreen(
     val entries by viewModel.entries.collectAsState()
 
     val drawerState = rememberDrawerState(DrawerValue.Closed)
+    var showMarkAsReadConfirmationDialog by remember { mutableStateOf(false) }
 
     val coroutineScope = rememberCoroutineScope()
 
@@ -94,19 +98,30 @@ fun MainScreen(
 
                         item {
                             Button(
-                                onClick = {
-                                    coroutineScope.launch {
-                                        viewModel.performLoading {
-                                            viewModel.markPageAsRead()
-                                            viewModel.loadEntries()
-                                        }
-                                    }
-                                },
+                                onClick = { showMarkAsReadConfirmationDialog = true },
                                 modifier = Modifier.padding(16.dp)
                             ) {
                                 Text("Mark this page as read")
                             }
                         }
+                    }
+
+                    if (showMarkAsReadConfirmationDialog) {
+                        ConfirmationDialog(
+                            title = "Mark page as read",
+                            text = "Are you sure you want to mark this page as read?",
+                            confirmText = "Mark as read",
+                            dismissText = "Cancel",
+                            onConfirm = {
+                                coroutineScope.launch {
+                                    viewModel.performLoading {
+                                        viewModel.markPageAsRead()
+                                        viewModel.loadEntries()
+                                    }
+                                }
+                            },
+                            onRespond = { showMarkAsReadConfirmationDialog = false }
+                        )
                     }
                 } else {
                     Box(
