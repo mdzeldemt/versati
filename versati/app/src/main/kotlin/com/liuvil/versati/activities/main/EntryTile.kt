08c5d864869fdb7398795e28abd6589c99043f5b
Facundo Almeida <facundoalmeida@gmail.com>
2024-12-27 21:33:24 -0300

    Fix entry tile not taking all available width

--- versati/app/src/main/kotlin/com/liuvil/versati/activities/main/EntryTile.kt
+++ versati/app/src/main/kotlin/com/liuvil/versati/activities/main/EntryTile.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -47,7 +48,10 @@ fun EntryTile(
     }
 
     Box(
-        if (isRead) Modifier.alpha(0.5f) else Modifier
+        Modifier.fillMaxWidth()
+            .apply {
+                if (isRead) alpha(0.5f)
+            }
     ) {
         imageUrl?.let {
             WrapperLayout(
