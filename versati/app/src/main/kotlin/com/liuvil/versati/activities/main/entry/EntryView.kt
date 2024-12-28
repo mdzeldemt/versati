d016022a3726ddcc26c763a3a340e56105ed51f2
Facundo Almeida <facundoalmeida@gmail.com>
2024-12-27 21:20:32 -0300

    Fix layout of entry title and entry details on entry view

--- versati/app/src/main/kotlin/com/liuvil/versati/activities/main/entry/EntryView.kt
+++ versati/app/src/main/kotlin/com/liuvil/versati/activities/main/entry/EntryView.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -77,15 +78,18 @@ fun EntryView(
                     it.title,
                     fontSize = 20.sp,
                     fontWeight = FontWeight.Bold,
-                    modifier = Modifier.clickable(
-                        onClick = openURL,
-                        indication = null,
-                        interactionSource = null
-                    )
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable(
+                            onClick = openURL,
+                            indication = null,
+                            interactionSource = null
+                        )
                 )
 
                 Text(
-                    getEntryDetailsText(it.feedTitle, it.author, it.publishedAt)
+                    getEntryDetailsText(it.feedTitle, it.author, it.publishedAt),
+                    modifier = Modifier.fillMaxWidth()
                 )
 
                 enclosure?.let { enclosure ->
