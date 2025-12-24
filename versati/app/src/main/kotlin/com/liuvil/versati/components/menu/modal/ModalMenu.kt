cd0d94cdcfca8492ca947f7020fbb436c36157c0
Facundo Almeida <falmeida@orangeloops.com>
2025-12-24 15:58:38 -0300

    Update modal menu appearance

--- versati/app/src/main/kotlin/com/liuvil/versati/components/menu/modal/ModalMenu.kt
+++ versati/app/src/main/kotlin/com/liuvil/versati/components/menu/modal/ModalMenu.kt
@@ -1,19 +1,19 @@
 package com.liuvil.versati.components.menu.modal
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.DialogProperties
+import androidx.compose.ui.window.Dialog
 
 data class ModalMenuItem(
     val title: String,
@@ -25,16 +25,16 @@ fun ModalMenu(
     items: List<ModalMenuItem>,
     onDismiss: () -> Unit
 ) {
-    AlertDialog(
-        properties = DialogProperties(
-            usePlatformDefaultWidth = false
-        ),
-        onDismissRequest = {
-            onDismiss()
-        },
-        text = {
+    Dialog(
+        onDismissRequest = onDismiss
+    ) {
+        Surface(
+            shape = MaterialTheme.shapes.large,
+            tonalElevation = 6.dp
+        ) {
             LazyColumn(
-                verticalArrangement = Arrangement.spacedBy(12.dp, Alignment.Top)
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(12.dp)
             ) {
                 items(items) {
                     Box(
@@ -52,7 +52,6 @@ fun ModalMenu(
                     }
                 }
             }
-        },
-        confirmButton = {}
-    )
+        }
+    }
 }
