8d657a050cf86d5e203bf35efb8d2e106834e25d
Facundo Almeida <facundoalmeida@gmail.com>
2026-03-02 23:40:43 -0300

    Fix concurrency issues on cache database

--- versati/app/src/main/kotlin/com/liuvil/versati/repository/cache/di/CacheDatabaseModule.kt
+++ versati/app/src/main/kotlin/com/liuvil/versati/repository/cache/di/CacheDatabaseModule.kt
@@ -10,20 +10,22 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import java.io.File
+import javax.inject.Singleton
 
 private const val CACHE_DATABASE_NAME = "network_cache_db"
 
 @Module
 @InstallIn(SingletonComponent::class)
-class CacheDatabaseModule {
+object CacheDatabaseModule {
     @Provides
+    @Singleton
     fun provideCacheDatabase(
         @ApplicationContext context: Context
     ): CacheDatabase {
         val cacheDir = context.cacheDir
         val databaseFile = File(cacheDir, CACHE_DATABASE_NAME)
         return Room.databaseBuilder(context, CacheDatabase::class.java, databaseFile.absolutePath)
-            .setJournalMode(RoomDatabase.JournalMode.TRUNCATE)
+            .setJournalMode(RoomDatabase.JournalMode.WRITE_AHEAD_LOGGING)
             .fallbackToDestructiveMigration()
             .build()
     }
